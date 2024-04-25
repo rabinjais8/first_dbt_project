b9a57f331b327ac5147537433de63d481c92a400
Rabindra Jaiswal <rabinjais8@gmail.com>
2024-04-25 12:13:57 +0000

    next commit 10

--- analyses/test_debug_1.sql
+++ analyses/test_debug_1.sql
@@ -1,13 +1,6 @@
 select
-      count(*) as failures,
-      count(*) != 0 as should_warn,
-      count(*) != 0 as should_error
-    from (
-      
-    select
-    orderid as unique_field,
-    count(*) as n_records
-from analytics.dbt_rjaiswal.raw_orders
-where orderid is not null
-group by orderid
-having count(*) > 1)
+        shipmode as value_field,
+        count(*) as n_records
+
+    from analytics.dbt_rjaiswal.raw_orders
+    group by shipmode
